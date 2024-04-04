95948995d21b97e925238a748e6b7bd881cf578a
YenPhan1708 <yen.phan@student.nhlstenden.com>
2024-04-04 11:31:30 +0200

    Solve the maze but a little bit off track

--- LineMaze/project_trial10/project_trial10.ino
+++ LineMaze/project_trial10/project_trial10.ino
@@ -35,8 +35,7 @@ int motorSpeedForward = 255;
 int motorSpeedTurn = 255;
 
 int lineCount = 0;  
-bool shouldPerformLineCountingAndGrabbing = true;
-int executionStage = 0;  
+bool shouldPerformLineCountingAndGrabbing = true;  
 
 volatile long countL = 0;
 volatile long countR = 0;
@@ -60,11 +59,16 @@ void turnRight();
 void grabObject();
 void releaseObject();
 void controlGripper(int pulseWidth);
-int calculateLineThreshold();
 void performLineCountingAndGrabbing();
 void measureAndAdjustSpeed();
 bool blackLineDetectedFor2Seconds();
 float getDistance();
+void colorTurnLeft();
+void colorTurnRight();
+void colorForward();
+void colorStop();
+void getSensorsValues();
+void solveMaze();
 
 void setup() {
   strip.begin();
@@ -95,33 +99,38 @@ void setup() {
 }
 
 
-
 void loop() {
   getSensorsValues();
   calculateLineThreshold();
-//  if(executionStage == 0){
-  // Check if line counting and grabbing should be performed
-    if (shouldPerformLineCountingAndGrabbing) {
-      performLineCountingAndGrabbing();
-    } 
-    else {
-      // Check if all sensors detect black (indicating the presence of the black box)
-//      if (sensors[0] && sensors[1] && sensors[2] && sensors[3] && sensors[4] && sensors[5] && sensors[6] && sensors[7]) {
-//         blackLineDetectedFor2Seconds();
-         solveMaze();
-//        }   
-       }
+  if (shouldPerformLineCountingAndGrabbing) { //this part of code makes sure to run this only ones and then ignore
+    performLineCountingAndGrabbing();
+  }
+  else 
+  {
+//      if( sensors[0] && sensors[1] && sensors[2] && sensors[3] && sensors[4]&& sensors[5] && sensors[6] && sensors[7]){
+//      Serial.print("YES");
+//      moveForward();
+//      if(sensors[0] && sensors[1] && sensors[2] && sensors[3] && sensors[4]&& sensors[5] && sensors[6] && sensors[7]){
+//          moveForward();
+//        // Step back a little bit   
+//          analogWrite(leftP, 100);
+//          analogWrite(leftN, 255);
+//          analogWrite(rightP, 100);
+//          analogWrite(rightN, 255);
+//          delay(500); // Adjust the delay based on how much you want the robot to step back
+//        
+//          // Drop the object (open the gripper)
+//          releaseObject();
+//          delay(1000); // Delay to showcase the drop action
+//         
+//          // Stop the robot
+//          stop();
+//    }
+//   }
+//    else {
+      solveMaze(); 
     }
-//    else if(executionStage == 1){
-//      releaseObject(); 
-//      moveBackward();
-//      executionStage = 2;
-//    }
-//    else if (executionStage == 2){
-//      executionStage = 2;
-//      stopMotors();
-//    }
-//}
+  }
 
 
 
@@ -131,10 +140,10 @@ void turnUntilMeetTheLine() {
   int sensorValue0, sensorValue1, sensorValue2, sensorValue3, sensorValueA7, sensorValueA5;
 
   // Start turning
-  analogWrite(rightP, 20);
+  analogWrite(rightP, 40);
   analogWrite(rightN, motorSpeedTurn); 
   analogWrite(leftP, motorSpeedTurn);
-  analogWrite(leftN, 20);
+  analogWrite(leftN, 40);
 
   // Continuously check sensor values
   do {
@@ -150,40 +159,28 @@ void turnUntilMeetTheLine() {
   } while (!((sensorValue0 > calculateLineThreshold() || sensorValue1 > calculateLineThreshold() || sensorValue2 > calculateLineThreshold() || sensorValue3 > calculateLineThreshold()) &&
              sensorValueA7 < calculateLineThreshold() && sensorValueA5 < calculateLineThreshold()));
 
-  stopMotors();
+//  stop();
 }
 
 void turnRight(){
   colorTurnRight();
   countL=0;
   countR=0;
-  while(!(sensors[4] || sensors[3])||(countL < 10 && countR < 10)){    
-    analogWrite(leftP, 20);
-    analogWrite(leftN, 200);
-    analogWrite(rightP, 200);
-    analogWrite(rightN, 20);
+  while(countL < 25 && countR < 25){
+    analogWrite(leftP, 55);
+    analogWrite(leftN, 255);
+    analogWrite(rightP, 255);
+    analogWrite(rightN, 55);
   }
+  stop();
 }
-
-void turnLeft(){
-  colorTurnLeft();
-  countL=0;
-  countR=0;
-  while(!(sensors[4] || sensors[3])||(countL < 10 && countR < 10)){    
-    analogWrite(leftP, 200);
-    analogWrite(leftN, 20);
-    analogWrite(rightP, 20);
-    analogWrite(rightN, 20);
-  }
-}
-
 
 
 void moveForwardBeforeTurn(){
   colorForward();
   countL=0;
   countR=0;
-  while(countL < 12 && countR < 12){
+  while(countL < 10 && countR < 10){
     analogWrite(leftP, 255);
     analogWrite(leftN, 10);
     analogWrite(rightP, 255);
@@ -194,25 +191,18 @@ void moveForwardBeforeTurn(){
 void moveForward() { //Function to move forward
   colorForward();
   analogWrite(leftP, motorSpeedForward);
-  analogWrite(leftN, 0);
+  analogWrite(leftN, 10);
   analogWrite(rightP, motorSpeedForward);
-  analogWrite(rightN, 0);
+  analogWrite(rightN, 10);
 }
 
-void stopMotors(){
+void stop(){
     analogWrite(leftP, 0);
     analogWrite(leftN, 0);
     analogWrite(rightN, 0);
     analogWrite(rightP, 0);
     delay(500);
     }
-
-void moveBackward() {
-    analogWrite(leftP, 10);
-    analogWrite(leftN, 255);
-    analogWrite(rightN, 10);
-    analogWrite(rightP, 255);
-}
 
 //Function to calculate the Threshold value
 
@@ -273,7 +263,7 @@ void colorTurnRight()
 } 
 
 //All red
-void colorstop()
+void colorStop()
 {
    for(int i = 0; i < strip.numPixels(); i++) {
     strip.setPixelColor(i, strip.Color(0,255,0));
@@ -305,7 +295,7 @@ void solveMaze(){
       if (turnL==0){
         moveForwardBeforeTurn();
         }     
-      turnLeft();
+      turnUntilMeetTheLine();
       turnL++;
       }
     else{
@@ -313,51 +303,51 @@ void solveMaze(){
     if(sensors[6]){
     colorForward();
     analogWrite(rightP, 255);
-    analogWrite(rightN, 0);
+    analogWrite(rightN, 10);
     analogWrite(leftP, 140);
-    analogWrite(leftN, 0);
+    analogWrite(leftN, 10);
     }
     else if(sensors[1]){
     colorTurnRight();
     analogWrite(rightP, 140);
-    analogWrite(rightN, 0);
+    analogWrite(rightN, 10);
     analogWrite(leftP, 255);
-    analogWrite(leftN, 0);
+    analogWrite(leftN, 10);
     }
     else if(sensors[5]){
     colorForward();
     analogWrite(rightP, 255);
-    analogWrite(rightN, 0);
+    analogWrite(rightN, 10);
     analogWrite(leftP, 230);
-    analogWrite(leftN, 0);
+    analogWrite(leftN, 10);
     }
     else if(sensors[2]){
     colorTurnLeft();
     analogWrite(rightP, 230);
-    analogWrite(rightN, 0);
+    analogWrite(rightN, 10);
     analogWrite(leftP, 255);
-    analogWrite(leftN, 0);
+    analogWrite(leftN, 10);
     }
     else if(sensors[4] && !sensors[3]){
     colorTurnRight();
     analogWrite(rightP, 255);
-    analogWrite(rightN, 0);
+    analogWrite(rightN, 10);
     analogWrite(leftP, 240);
-    analogWrite(leftN, 0);
+    analogWrite(leftN, 10);
     }
     else if(sensors[3] && !sensors[4]){
     colorTurnLeft();
     analogWrite(rightP, 240);
-    analogWrite(rightN, 0);
+    analogWrite(rightN, 10);
     analogWrite(leftP, 255);
-    analogWrite(leftN, 0);
+    analogWrite(leftN, 10);
     }
     else{
     colorForward();
     analogWrite(rightP, 255);
-    analogWrite(rightN, 0);
+    analogWrite(rightN, 10);
     analogWrite(leftP, 255);
-    analogWrite(leftN, 0);
+    analogWrite(leftN, 10);
     }
    }
  }
@@ -398,10 +388,14 @@ void performLineCountingAndGrabbing() { //Function for grabbing and starting
     }
   }
    if (lineCount == 4 ) {
-    stopMotors();
+    stop();
     grabObject();
     delay(500); // Delay to showcase the grab action
-    turnUntilMeetTheLine();// Turn left 90 degrees after grabbing the object
+//    turnUntilMeetTheLine();// Turn left 90 degrees after grabbing the object
+    analogWrite(rightP, 240);
+    analogWrite(rightN, 10);
+    analogWrite(leftP, 10);
+    analogWrite(leftN, 10);
     turnL++;
     shouldPerformLineCountingAndGrabbing = false;
     lineCount++; // Increment to prevent re-entering this block
@@ -428,51 +422,33 @@ void controlGripper(int pulseWidth) { //Function to control gripper without Serv
 
 //Function to drop the object and finish the maze
 
-//void dropObjectAndstopMotors() {
-//    // Move forward to check if the box is still black
-//    moveForward();
+//void dropObjectAndStop() {
+//  
+//  getSensorsValues();
+  // Check if all line sensors detect black
+  
+//    if (!sensors[0] || !sensors[1] || !sensors[2] || !sensors[3] || !sensors[4] || !sensors[5] || !sensors[6] || !sensors[7]) {
+//      break; // No need to continue checking if one sensor detects white
+//    } else if (sensors[0] && sensors[1] && sensors[2] && sensors[3] && sensors[4]&& sensors[5] && sensors[6] && sensors[7]) { // If all sensors detect black
 //    
-//    // Check if all sensors still detect black
-//    if (sensors[0] && sensors[1] && sensors[2] && sensors[3] && sensors[4] && sensors[5] && sensors[6] && sensors[7]) {
-//      // Step back a little bit
-//      analogWrite(leftP, 100);
-//      analogWrite(leftN, 255);
-//      analogWrite(rightP, 100);
-//      analogWrite(rightN, 255);
-//      delay(500); // Adjust the delay based on how much you want the robot to step back
+//      moveForwardBeforeTurn();
 //      
-//      // Drop the object (open the gripper)
-//      releaseObject();
-//      delay(1000); // Delay to showcase the drop action
+//      if(sensors[0] && sensors[1] && sensors[2] && sensors[3] && sensors[4]&& sensors[5] && sensors[6] && sensors[7]){
+//        // Step back a little bit
+//        
+//        analogWrite(leftP, 100);
+//        analogWrite(leftN, 255);
+//        analogWrite(rightP, 100);
+//        analogWrite(rightN, 255);
+//        delay(500); // Adjust the delay based on how much you want the robot to step back
 //      
-//      // Stop the robot
-//      stopMotors();
-//    }
+//        // Drop the object (open the gripper)
+//        releaseObject();
+//        delay(1000); // Delay to showcase the drop action
+//       
+//        // Stop the robot
+//        stop();
+//      } else {
+//        break;
+//      }
 //  }
-
-  bool blackLineDetectedFor2Seconds() {
-    static unsigned long startTime = 0; 
-    const long detectionPeriod = 500; 
-    bool allBlackDetected = true;
-
-    for (int i = 0; i < 8; i++) {
-        if (analogRead(sensors[i]) < calculateLineThreshold()) {
-            allBlackDetected = false;
-            break;
-        }
-    }
-
-    if (allBlackDetected) {
-        if (startTime == 0) {
-            startTime = millis();
-        } else if (millis() - startTime >= detectionPeriod) {
-            startTime = 0; 
-            executionStage = 1;
-            return true; 
-        }
-    } else {
-        startTime = 0;
-    }
-
-    return false;
-}
